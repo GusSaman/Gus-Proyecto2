fb0215e9f33cb909332b52ed5dfbd1fd97658d5f
GusSaman <gustavo.saman@gmail.com>
2022-10-03 02:35:21 -0300

    iniciando Git

---
+++ app.js
@@ -0,0 +1,39 @@
+// let legacy = ["ADIDAS","NIKE","PUMA "]
+
+
+
+
+// function myFunction()
+
+
+// console.log("hola mundo")
+// let nuevo=prompt("quien se agrega?")
+
+// como agregar a una persona
+// array.puss= agrega un nuevo elemento a la lista
+
+// comision.push(nuevo)
+// console.log(comision)
+
+// como eliminar a una persona 
+//  comision.pop()
+
+// mi local 
+// let lista = ["PUMA", "ADIDAS"]
+// function agregar (){
+//     let nuevoProducto = prompt("que desea agregar?")
+//     lista.push(nuevoProducto)
+// }
+// function eliminar(){
+//     let posicionDeProducto = Number(prompt("que desea eliminar"))
+//      lista.splice ( posicionDeProducto,1)
+// }
+// function verLista(){
+// console.log(lista)
+
+// function buscarProducto(){
+//     let articulo = Number(prompt("posicion de producto"))
+//     let producto = lista[articulo]
+//     console.log(producto)
+// }
+
